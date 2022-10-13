ce375394f76d5c1ff6c273be9b054c920bbefdda
igor-86 <i.campoli86@gmail.com>
2022-10-13 22:14:58 +0200

    my first idea of ​​an online book shop - to be continued

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 /* Consegna:
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while. */
 
-const bookList = ["Il signore degli anelli - J.R.R. Tolkien", "Furore - John Steinbeck", "La dodicesima notte - William Shakespeare", "Casa di bambola - Henrik Ibsen", "Uno, nessuno e centomila - Luigi Pirandello"];
+const bookList = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
+const textList = ["Il signore degli anelli - J.R.R. Tolkien", "Furore - John Steinbeck", "La dodicesima notte - William Shakespeare", "Casa di bambola - Henrik Ibsen", "Uno, nessuno e centomila - Luigi Pirandello"];
 
-const listPage = document.querySelector(".lista");
-
+const listPage = document.querySelector(".row");
+const texListPage = document.querySelector(".sec");
 console.log(bookList);
 
 let i = 0;
 while(i < bookList.length) {
     let list = bookList[i];
     console.log(list);
+    let tex = textList[i];
+    console.log(tex);
     
-    const itemList = `<li>${list}</li>`;
+    const itemList = `
+            <div class="col">
+                <div class="books">
+                    <div class="image-books">
+                        <img src="${list}" alt="">
+                    </div>
+                    <div class="sec">
+                        <h3>${tex}</h3>                                            
+                    </div>
+                </div>
+            </div>
+    
+    `
+    
+    
+    /* const texstListPage = `<h2>${tex}</h2>`; */
     
     listPage.innerHTML += itemList;
+    /* texListPage.innerHTML += texstListPage; */
 
     i++;
     
